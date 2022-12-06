dcadbc6078271f190e56cf6eac53270919eb6ff7
i-muiri <ianmuiri99@gmail.com>
2022-12-06 15:09:45 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+const removingMain = document.querySelector('#main')
+removingMain.remove()
+const newHeader = document.createElement('h1')
+newHeader.id = 'victory'
+newHeader.innerHTML = 'Ian Muiri is the champion'
